ad11525a1271e848b4f97e330c13f9a74317546e
m-buch <122435577+m-buch@users.noreply.github.com>
2025-08-29 19:10:37 -0400

    Added tabs component

---
+++ src/components/ui/tabs/TabsList.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { reactiveOmit } from "@vueuse/core";
+import { TabsList } from "reka-ui";
+import { cn } from "@/lib/utils";
+
+const props = defineProps({
+  loop: { type: Boolean, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  class: { type: null, required: false },
+});
+
+const delegatedProps = reactiveOmit(props, "class");
+</script>
+
+<template>
+  <TabsList
+    data-slot="tabs-list"
+    v-bind="delegatedProps"
+    :class="
+      cn(
+        'bg-neutral-100 text-neutral-500 inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px] dark:bg-neutral-800 dark:text-neutral-400',
+        props.class,
+      )
+    "
+  >
+    <slot />
+  </TabsList>
+</template>
